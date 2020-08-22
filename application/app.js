deee3add0fc1b5e3fa05e1270b9f140d4b51855c
Valentin Aleksandrov <valentin.aleksandrov@tick42.com>
2020-08-22 15:33:19 +0300

    clean-up and translate to bulgarian the next button

--- application/app.js
+++ application/app.js
@@ -3,7 +3,6 @@ const Notes = function (selector, tuner) {
   this.isAutoMode = true;
   // this.$root = document.querySelector(selector);
   // this.$notesList = this.$root.querySelector(".notes-list");
-  // this.$frequency = this.$root.querySelector(".frequency");
   this.$notes = [];
   this.$notesMap = {};
   // this.createNotes();
@@ -11,9 +10,6 @@ const Notes = function (selector, tuner) {
 const Application = function () {
   this.tuner = new Tuner();
   this.notes = new Notes(".notes", this.tuner);
-  this.meter = new Meter(".meter");
-  this.frequencyBars = new FrequencyBars(".frequency-bars");
-  this.update({ name: "A", frequency: 440, octave: 4, value: 69, cents: 0 });
 };
 Application.prototype.start = function () {
   const self = this;
@@ -21,39 +17,16 @@ Application.prototype.start = function () {
   this.tuner.onNoteDetected = function (note) {
     if (self.notes.isAutoMode) {
       if (self.lastNote === note.name) {
-        console.log("------>", note);
-        self.update(note);
+        console.log("note --> ", note);
       } else {
-        // console.warn("ELSE statement note", note);
         self.lastNote = note.name;
       }
     }
   };
 
-  swal("Welcome online tuner!").then(function () {
+  swal("Натиснете Продължи за да започнете обучението...").then(function () {
     self.tuner.init();
-    self.frequencyData = new Uint8Array(self.tuner.analyser.frequencyBinCount);
   });
-
-  this.updateFrequencyBars();
-};
-
-Application.prototype.updateFrequencyBars = function () {
-  if (this.tuner.analyser) {
-    this.tuner.analyser.getByteFrequencyData(this.frequencyData);
-    this.frequencyBars.update(this.frequencyData);
-  }
-  requestAnimationFrame(this.updateFrequencyBars.bind(this));
-};
-
-Application.prototype.update = function (note) {
-  // this.notes.update(note);
-  this.meter.update((note.cents / 50) * 45);
-};
-
-// noinspection JSUnusedGlobalSymbols
-Application.prototype.toggleAutoMode = function () {
-  // this.notes.toggleAutoMode();
 };
 
 const app = new Application();
